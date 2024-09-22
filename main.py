a6021dbcc27237d9af2a4edcef47b8f38a379c0f
GD Hiruna <hirunaofficial@gmail.com>
2024-09-22 08:06:27 +0530

    Added persistent subscription storage with JSON file support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import json
 from telebot import types
 
 # Replace with your bot's token
@@ -6,7 +7,7 @@ TOKEN = 'your_telegram_bot_token'
 
 bot = telebot.TeleBot(TOKEN)
 
-user_subscriptions = {}
+SUBSCRIPTION_FILE = 'subscriptions.json'
 
 district_results = {
     "colombo": "Colombo Results: Party A - 40%, Party B - 35%, Party C - 25%",
@@ -14,6 +15,19 @@ district_results = {
 }
 
 overall_results = "Overall Results: Party A - 45 seats, Party B - 40 seats, Party C - 15 seats"
+
+def load_subscriptions():
+    try:
+        with open(SUBSCRIPTION_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_subscriptions(subscriptions):
+    with open(SUBSCRIPTION_FILE, 'w') as file:
+        json.dump(subscriptions, file)
+
+user_subscriptions = load_subscriptions()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -37,11 +51,12 @@ def send_overall_result(message):
 @bot.message_handler(commands=['subscribe'])
 def subscribe_to_updates(message):
     try:
-        user_id = message.from_user.id
+        user_id = str(message.from_user.id)
         district_name = message.text.split()[1].lower()
         
         if district_name in district_results:
             user_subscriptions[user_id] = district_name
+            save_subscriptions(user_subscriptions)  # Save subscription to JSON
             bot.reply_to(message, f"You have subscribed to updates for {district_name}.")
         else:
             bot.reply_to(message, "Sorry, I don't have results for that district.")
@@ -50,9 +65,10 @@ def subscribe_to_updates(message):
 
 @bot.message_handler(commands=['unsubscribe'])
 def unsubscribe_from_updates(message):
-    user_id = message.from_user.id
+    user_id = str(message.from_user.id)
     if user_id in user_subscriptions:
         del user_subscriptions[user_id]
+        save_subscriptions(user_subscriptions)  # Save updated subscriptions to JSON
         bot.reply_to(message, "You have been unsubscribed from updates.")
     else:
         bot.reply_to(message, "You are not subscribed to any updates.")
